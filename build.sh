6ef1da0fc09fb804d26980c844d2c1bad928a5e6
Andreyka445 <seleznevandrey186@outlook.com>
2025-11-17 20:55:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
-rm -rf device/tecno/LH7n &&
+rm -rf device/tecno/CK7n &&
 rm -rf device/tecno/mt6789-common &&
-rm -rf device/tecno/LH7n-kernel &&
-rm -rf vendor/tecno/LH7n &&
+rm -rf device/tecno/CK7n-kernel &&
+rm -rf vendor/tecno/CK7n &&
 rm -rf vendor/tecno/mt6789-common &&
 rm -rf vendor/sony/dolby &&
 rm -rf vendor/JamesDSP &&
@@ -23,7 +23,7 @@ rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x86 &&
 
 #Clone the deivce manifest
-git clone https://github.com/Andreyka445/local_manifests.git -b mistos-16-lh7n .repo/local_manifests
+git clone https://github.com/Andreyka445/local_manifests.git -b mistos-16-ck7n .repo/local_manifests
 
 #initialize rom repo
 repo init -u https://github.com/Project-Mist-OS/manifest -b 4.3 --git-lfs --depth=1
@@ -36,6 +36,6 @@ git clone --depth=1 --branch main https://github.com/Andreyka445/signingkey vend
 
 #Setup environment and start build
 . build/envsetup.sh &&
-mistify LH7n userdebug &&
+mistify CK7n userdebug &&
 mist b
 "
