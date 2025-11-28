b7569714b1fd47897c79367316389288a604a6b4
Andreyka445 <seleznevandrey186@outlook.com>
2025-11-28 13:07:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-
 set -e
 
 crave run --no-patch -- "
 #Nuke dirs
-rm -rf .repo/local_manifests
-rm -rf device/tecno/LH7n
-rm -rf device/tecno/mt6789-common
-rm -rf device/tecno/LH7n-kernel
-rm -rf vendor/tecno/LH7n
-rm -rf vendor/tecno/mt6789-common
-rm -rf vendor/sony/dolby
-rm -rf vendor/JamesDSP
-rm -rf packages/apps/ViPER4AndroidFX
-rm -rf hardware/mediatek
+rm -rf .repo/local_manifests &&
+rm -rf device/tecno/LH7n &&
+rm -rf device/tecno/mt6789-common &&
+rm -rf device/tecno/LH7n-kernel &&
+rm -rf vendor/tecno/LH7n &&
+rm -rf vendor/tecno/mt6789-common &&
+rm -rf vendor/sony/dolby &&
+rm -rf vendor/JamesDSP &&
+rm -rf packages/apps/ViPER4AndroidFX &&
+rm -rf hardware/mediatek &&
 rm -rf hardware/transsion
-rm -rf device/mediatek/sepolicy_vndr
-rm -rf vendor/lineage-priv/keys
-rm -rf vendor/evolution-priv/keys
-rm -rf vendor/lmodroid-priv/keys
-rm -rf build/soong
-rm -rf vendor/google/gms
-rm -rf vendor/gms
-rm -rf prebuilts/clang/host/linux-x86
-rm -rf platform/prebuilts/clang/host/linux-x86
+rm -rf device/mediatek/sepolicy_vndr &&
+rm -rf vendor/lineage-priv/keys &&
+rm -rf vendor/evolution-priv/keys &&
+rm -rf vendor/lmodroid-priv/keys &&
+rm -rf build/soong &&
+rm -rf vendor/google/gms &&
+rm -rf vendor/gms &&
+rm -rf prebuilts/clang/host/linux-x86 &&
+rm -rf platform/prebuilts/clang/host/linux-x86 &&
 
 echo "==> Syncing sources..."
 
