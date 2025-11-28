9212c6ffe65f0eac4500d6b998c1b970b4393842
Andreyka445 <seleznevandrey186@outlook.com>
2025-11-28 12:43:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 removedir=(
-.repo/local_manifests &&
-device/tecno/LH7n &&
-device/tecno/mt6789-common &&
-device/tecno/LH7n-kernel &&
-vendor/tecno/LH7n &&
-vendor/tecno/mt6789-common &&
-vendor/sony/dolby &&
-vendor/JamesDSP &&
-packages/apps/ViPER4AndroidFX &&
-hardware/mediatek &&
-hardware/transsion &&
-device/mediatek/sepolicy_vndr &&
-vendor/lineage-priv/keys &&
-vendor/evolution-priv/keys &&
-vendor/lmodroid-priv/keys &&
-build/soong &&
-vendor/google/gms &&
-vendor/gms &&
-prebuilts/clang/host/linux-x86 &&
-platform/prebuilts/clang/host/linux-x86 &&
+.repo/local_manifests
+device/tecno/LH7n
+device/tecno/mt6789-common
+device/tecno/LH7n-kernel
+vendor/tecno/LH7n
+vendor/tecno/mt6789-common
+vendor/sony/dolby
+vendor/JamesDSP
+packages/apps/ViPER4AndroidFX
+hardware/mediatek
+hardware/transsion
+device/mediatek/sepolicy_vndr
+vendor/lineage-priv/keys
+vendor/evolution-priv/keys
+vendor/lmodroid-priv/keys
+build/soong
+vendor/google/gms
+vendor/gms
+prebuilts/clang/host/linux-x86
+platform/prebuilts/clang/host/linux-x86
 )
 
 rm -rf "${removedir[@]}"
