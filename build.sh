80b88eaca6293f0a5ca642a12051dba25747b03d
Andreyka445 <seleznevandrey186@outlook.com>
2025-11-28 12:59:53 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-removedir=(
-.repo/local_manifests
-device/tecno/LH7n
-device/tecno/mt6789-common
-device/tecno/LH7n-kernel
-vendor/tecno/LH7n
-vendor/tecno/mt6789-common
-vendor/sony/dolby
-vendor/JamesDSP
-packages/apps/ViPER4AndroidFX
-hardware/mediatek
-hardware/transsion
-device/mediatek/sepolicy_vndr
-vendor/lineage-priv/keys
-vendor/evolution-priv/keys
-vendor/lmodroid-priv/keys
-build/soong
-vendor/google/gms
-vendor/gms
-prebuilts/clang/host/linux-x86
-platform/prebuilts/clang/host/linux-x86
-)
+#Nuke dirs
+rm -rf.repo/local_manifests
+rm -rf device/tecno/LH7n
+rm -rf device/tecno/mt6789-common
+rm -rf device/tecno/LH7n-kernel
+rm -rf vendor/tecno/LH7n
+rm -rf vendor/tecno/mt6789-common
+rm -rf vendor/sony/dolby
+rm -rf vendor/JamesDSP
+rm -rf packages/apps/ViPER4AndroidFX
+rm -rf hardware/mediatek
+rm -rf hardware/transsion
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf vendor/lineage-priv/keys
+rm -rf vendor/evolution-priv/keys
+rm -rf vendor/lmodroid-priv/keys
+rm -rf build/soong
+rm -rf vendor/google/gms
+rm -rf vendor/gms
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf platform/prebuilts/clang/host/linux-x86
 
-rm -rf "${removedir[@]}"
+echo "==> Syncing sources..."
 
 #Clone the deivce manifest
 git clone https://github.com/Andreyka445/local_manifests.git -b evox-16-lh7n .repo/local_manifests
@@ -35,11 +34,15 @@ repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
 /opt/crave/resync.sh
 
 #Signing
-git clone --depth=1 --branch evolution https://github.com/Andreyka445/signingkey vendor/evolution-priv/keys &&
+git clone --depth=1 --branch evolution https://github.com/Andreyka445/signingkey vendor/evolution-priv/keys
+
+echo "=== Starting Build ==="
 
 #Setup environment and start build
 . build/envsetup.sh
 lunch lineage_LH7n-bp3a-userdebug
 make installclean
-m evolution 
-"
+m evolution
+mv out/target/product/LH7n
+
+echo "Build complete Successful"
