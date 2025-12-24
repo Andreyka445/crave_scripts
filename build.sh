5c918fa270c570c00fe3160b049b495997e7fe11
LORD | Andreyka445 <seleznevandrey186@outlook.com>
2025-12-24 08:07:46 +0300

    clover bringup
    Signed-off-by: Andreyka445 <seleznevandrey186@outlook.com> by LORD | Andreyka445

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ rm -rf hardware/transsion &&
 rm -rf device/mediatek/sepolicy_vndr &&
 rm -rf vendor/lineage-priv/keys &&
 rm -rf vendor/evolution-priv/keys &&
-rm -rf vendor/lmodroid-priv/keys &&
+rm -rf vendor/clover-priv/keys &&
 rm -rf device/qcom/sepolicy_vndr &&
 rm -rf build/soong &&
 rm -rf vendor/google/gms &&
@@ -27,22 +27,22 @@ rm -rf platform/prebuilts/clang/host/linux-x86 &&
 echo "==> Syncing sources..."
 
 #Clone the deivce manifest
-git clone https://github.com/Andreyka445/local_manifests.git -b evox-16-lh7n .repo/local_manifests
+git clone https://github.com/Andreyka445/local_manifests.git -b clover-16-lh7n .repo/local_manifests
 
 #initialize rom repo
-repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr1 --git-lfs
 
 #Sync
 /opt/crave/resync.sh
 
 #Signing
-git clone --depth=1 --branch evolution https://github.com/Andreyka445/signingkey vendor/evolution-priv/keys
+git clone --depth=1 --branch clover https://github.com/Andreyka445/signingkey vendor/clover-priv/keys
 
 echo "=== Starting Build ==="
 
 #Setup environment and start build
 . build/envsetup.sh
-lunch lineage_LH7n-bp3a-userdebug
+lunch clover_LH7n-bp3a-userdebug
 make installclean
-m evolution
+mka clover
 "
